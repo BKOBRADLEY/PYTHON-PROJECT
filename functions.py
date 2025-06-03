4747107a96b2dc9616df6d1c80a28d86f192adee
bradley <CoderBradleyOjijo@gmail.com>
2025-06-03 20:17:00 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,11 @@
+def add():
+    a=10
+    b=20
+    print(a+b)
+add()
+def sub(a,b):
+    print(a-b)
+c=int(input("enter number above forty"))
+d=int(input("enter number below forty"))
+sub(c,d)
+sub(d,c)
