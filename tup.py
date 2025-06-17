20fdfc8e2aad761a9c3bf6d989e52a8a98958cd6
bradley <CoderBradleyOjijo@gmail.com>
2025-06-17 20:18:24 +0300

    Add files via upload

---
+++ tup.py
@@ -0,0 +1,17 @@
+a= (1, 11, 21, 31, 41)
+print(a)
+print(type(a))
+a[3]
+print(a[3])
+print(a)
+b=[1, 11, 21, 31, ]
+print(type(b))
+b[3]
+print(b[3])
+b.append(41)
+b.insert(4,7)
+print(b)
+b.remove(7)
+print(b)
+b.pop(2)
+print(b)
