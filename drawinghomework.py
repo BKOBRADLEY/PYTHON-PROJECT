db513e9cd909902c8cfa8c91946b73b8561159f5
bradley <CoderBradleyOjijo@gmail.com>
2025-07-11 19:19:43 +0300

    Add files via upload

---
+++ drawinghomework.py
@@ -0,0 +1,19 @@
+import turtle
+srn=turtle.Screen()
+srn2= turtle.Turtle()
+srn3= turtle.Screen()
+srn4= turtle.Turtle()
+
+srn2.color("red")
+srn2.begin_fill()
+srn2.forward(40)
+srn2.right(25)
+srn2.forward(40)
+srn2.end_fill()
+srn.exitonclick()
+srn4.color("orange")
+srn4.begin_fill()
+srn4.forward(80)
+srn4.circle
+srn4.end_fill()
+srn3.exitonclick()
